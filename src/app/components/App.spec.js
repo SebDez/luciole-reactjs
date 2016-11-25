04e9e1a8631cc2aa6f40448e1a2379f38ad1ac79
SebDez <sebdez.dev@gmail.com>
2016-11-25 21:18:49 +0100

    clean project

--- src/app/components/App.spec.js
+++ src/app/components/App.spec.js
@@ -1,44 +1,11 @@
-/* import React from 'react';
+import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
 import {App} from './App';
-import Footer from './Footer';
-import Header from './Header';
 
 describe('App', () => {
 
-  const props = {
-    actions:{},
-    authActions:{
-      logUser:()=>{return 0;},
-      disconnectUser:()=>{return 0;}
-    },
-    auth:{user:{token:'token'}}
-  };
-
-  describe('Render', () => {
-    it('Should have appContainer class', () => {
-      const wrapper = shallow(<App {...props}/>);
-      expect(wrapper.find('.appContainer')).to.have.length(1);
-    });
-
-    it('should contain <Footer />', () => {
-      const wrapper = shallow(<App {...props}/>);
-      expect(wrapper.find(Footer)).to.be.length(1);
-    });
-
-    it('should contain <Header />', () => {
-      const wrapper = shallow(<App {...props}/>);
-      expect(wrapper.find(Header)).to.be.length(1);
-    });
-  });
-
-  describe('mapStateToProps', () => {
-    it('Should return object with user', () => {
-      const state = {auth:{user:'user1'}}
-      expect(App.mapStateToProps(state).auth.user).to.equal('user1');
-    });
-  });
+  const props = {};
 
   describe('mapDispatchToProps', () => {
     it('Should return empty object', () => {
@@ -46,4 +13,4 @@ describe('App', () => {
     });
   });
 
-}); */
+});
