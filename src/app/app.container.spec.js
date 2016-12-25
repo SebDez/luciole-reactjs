1fd71c8c361f7b8df4c92da1ccb563929f2980e0
SebDez <sebdez.dev@gmail.com>
2016-12-25 20:33:03 +0100

    trying to debug travis

--- src/app/app.container.spec.js
+++ src/app/app.container.spec.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
-import { App } from './app.container'
+import { App } from './app.container.js'
 import ReactSidebar from 'react-sidebar'
 
 describe('App', () => {
