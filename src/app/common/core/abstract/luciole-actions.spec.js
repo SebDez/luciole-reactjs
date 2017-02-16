ec9eca9376863bf2a3f6d74ede3b597fe41ea292
SebDez <sebdez.dev@gmail.com>
2017-02-16 21:10:16 +0100

    Unit tests for api service and luciole actions

--- src/app/common/core/abstract/luciole-actions.spec.js
+++ src/app/common/core/abstract/luciole-actions.spec.js
@@ -22,4 +22,44 @@ describe('LucioleActions', () => {
       expect(spy).to.have.been.called.with('error')
     })
   })
+
+  describe('getEnvConfFromGetState', () => {
+    var serv, getState
+    beforeEach(() => {
+      serv = new LucioleActions()
+      getState = () => {
+        return {
+          application: {
+            app: {
+              conf: 'my-conf'
+            }
+          }
+        }
+      }
+    })
+
+    it('Expect to return conf', () => {
+      expect(serv.getEnvConfFromGetState(getState)).to.equal('my-conf')
+    })
+  })
+
+  describe('getTokenFromGetState', () => {
+    var serv, getState
+    beforeEach(() => {
+      serv = new LucioleActions()
+      getState = () => {
+        return {
+          application: {
+            auth: {
+              token: 'my-token'
+            }
+          }
+        }
+      }
+    })
+
+    it('Expect to return conf', () => {
+      expect(serv.getTokenFromGetState(getState)).to.equal('my-token')
+    })
+  })
 })
